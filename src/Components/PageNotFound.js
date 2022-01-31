b9b526fb6b3c276c5acd6dcff8d7b6d312b8d5fa
munawwar <Munawwar1803@gmail.com>
2022-01-31 01:40:53 +0000

    "404"

--- src/Components/PageNotFound.js
+++ src/Components/PageNotFound.js
@@ -6,7 +6,19 @@ export default function PageNotFound() {
     style={{
       "margin":"none",
     "fontSize":"20vw",
-    "textAlign":"center"
+    "textAlign":"center",
+    "height":"10vh"
     }}>404</h1>
+    <p
+    style={{
+      "margin":"none",
+    "fontSize":"6vw",
+    "textAlign":"center",
+    "position":"relative",
+    "top":"-130px",
+    "color":"red",
+    "font-weight":"900"
+
+    }}>Page Not Found</p>
   </div>;
 }
